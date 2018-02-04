7afb331b5f73e2dbb07c5b1d69a6662d3cdfa277
takkyuuplayer <takkyuuplayer@gmail.com>
2018-02-04 00:25:15 -0500

    Chap11: Remove unnecessary tests

--- money/money_test.go
+++ money/money_test.go
@@ -18,20 +18,10 @@ func TestEquality(t *testing.T) {
 	assert.Equal(t, money.NewDoller(5).Equals(money.NewDoller(5)), true)
 	assert.Equal(t, money.NewDoller(5).Equals(money.NewDoller(6)), false)
 
-	assert.Equal(t, money.NewFranc(5).Equals(money.NewFranc(5)), true)
-	assert.Equal(t, money.NewFranc(5).Equals(money.NewFranc(6)), false)
-
 	assert.Equal(t, money.NewDoller(5).Equals(money.NewFranc(5)), false)
 }
 
 func TestCurrency(t *testing.T) {
 	assert.Equal(t, money.NewDoller(5).Currency(), "USD")
 	assert.Equal(t, money.NewFranc(5).Currency(), "CHF")
-}
-
-func TestFrancMultipulation(t *testing.T) {
-	five := money.NewFranc(5)
-
-	assert.Equal(t, money.NewFranc(10), five.Times(2))
-	assert.Equal(t, money.NewFranc(15), five.Times(3))
 }
