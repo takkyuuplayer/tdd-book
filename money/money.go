4371d4e3a2158c840587ca53fbb33af332d78559
takkyuuplayer <takkyuuplayer@gmail.com>
2018-02-03 22:15:16 -0500

    Add chap05

--- money/money.go
+++ money/money.go
@@ -16,3 +16,20 @@ func (d *Doller) Equals(obj interface{}) bool {
 	doller := obj.(*Doller)
 	return d.amount == doller.amount
 }
+
+type Franc struct {
+	amount int
+}
+
+func NewFranc(amount int) *Franc {
+	return &Franc{amount: amount}
+}
+
+func (d *Franc) Times(multiplier int) *Franc {
+	return NewFranc(d.amount * multiplier)
+}
+
+func (d *Franc) Equals(obj interface{}) bool {
+	doller := obj.(*Franc)
+	return d.amount == doller.amount
+}
--- money/money_test.go
+++ money/money_test.go
@@ -20,3 +20,17 @@ func TestEquality(t *testing.T) {
 	assert.Equal(t, five.Equals(money.NewDoller(5)), true)
 	assert.Equal(t, five.Equals(money.NewDoller(6)), false)
 }
+
+func TestFrancMultipulation(t *testing.T) {
+	five := money.NewFranc(5)
+
+	assert.Equal(t, money.NewFranc(10), five.Times(2))
+	assert.Equal(t, money.NewFranc(15), five.Times(3))
+}
+
+func TestFrancEquality(t *testing.T) {
+	five := money.NewFranc(5)
+
+	assert.Equal(t, five.Equals(money.NewFranc(5)), true)
+	assert.Equal(t, five.Equals(money.NewFranc(6)), false)
+}
