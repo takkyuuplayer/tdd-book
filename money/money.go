90cae61f126a72db27a1654ed07bc5b9a784689c
takkyuuplayer <takkyuuplayer@gmail.com>
2018-02-03 22:51:20 -0500

    Add chap06

--- money/money.go
+++ money/money.go
@@ -1,35 +1,50 @@
 package money
 
-type Doller struct {
+type Money struct {
 	amount int
+}
+
+type IMoney interface {
+	Amount() int
+}
+
+func (m *Money) Amount() int {
+	return m.amount
+}
+
+func (m *Money) Equals(obj interface{}) bool {
+	m2 := obj.(IMoney)
+	return m.Amount() == m2.Amount()
+}
+
+type Doller struct {
+	Money
+}
+
+func (d *Doller) amount() int {
+	return d.Money.amount
 }
 
 func NewDoller(amount int) *Doller {
-	return &Doller{amount: amount}
+	return &Doller{Money{amount: amount}}
 }
 
 func (d *Doller) Times(multiplier int) *Doller {
-	return NewDoller(d.amount * multiplier)
-}
-
-func (d *Doller) Equals(obj interface{}) bool {
-	doller := obj.(*Doller)
-	return d.amount == doller.amount
+	return NewDoller(d.amount() * multiplier)
 }
 
 type Franc struct {
-	amount int
+	Money
+}
+
+func (f *Franc) amount() int {
+	return f.Money.amount
 }
 
 func NewFranc(amount int) *Franc {
-	return &Franc{amount: amount}
+	return &Franc{Money{amount: amount}}
 }
 
 func (d *Franc) Times(multiplier int) *Franc {
-	return NewFranc(d.amount * multiplier)
-}
-
-func (d *Franc) Equals(obj interface{}) bool {
-	doller := obj.(*Franc)
-	return d.amount == doller.amount
+	return NewFranc(d.amount() * multiplier)
 }
