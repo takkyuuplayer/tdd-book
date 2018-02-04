b490d30a8100c72ded1d61f46d1ae30b402b9afe
takkyuuplayer <takkyuuplayer@gmail.com>
2018-02-03 23:02:06 -0500

    Skip chap07 because we cannot get type for struct embedding

--- money/money_test.go
+++ money/money_test.go
@@ -15,10 +15,13 @@ func TestMultipulation(t *testing.T) {
 }
 
 func TestEquality(t *testing.T) {
-	five := money.NewDoller(5)
+	assert.Equal(t, money.NewDoller(5).Equals(money.NewDoller(5)), true)
+	assert.Equal(t, money.NewDoller(5).Equals(money.NewDoller(6)), false)
 
-	assert.Equal(t, five.Equals(money.NewDoller(5)), true)
-	assert.Equal(t, five.Equals(money.NewDoller(6)), false)
+	assert.Equal(t, money.NewFranc(5).Equals(money.NewFranc(5)), true)
+	assert.Equal(t, money.NewFranc(5).Equals(money.NewFranc(6)), false)
+
+	// assert.Equal(t, money.NewDoller(5).Equals(money.NewFranc(5)), false)
 }
 
 func TestFrancMultipulation(t *testing.T) {
@@ -26,11 +29,4 @@ func TestFrancMultipulation(t *testing.T) {
 
 	assert.Equal(t, money.NewFranc(10), five.Times(2))
 	assert.Equal(t, money.NewFranc(15), five.Times(3))
-}
-
-func TestFrancEquality(t *testing.T) {
-	five := money.NewFranc(5)
-
-	assert.Equal(t, five.Equals(money.NewFranc(5)), true)
-	assert.Equal(t, five.Equals(money.NewFranc(6)), false)
 }
